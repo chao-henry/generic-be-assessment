63ad16e42ea83a1fb8d2455da2bba1bb277c94db
Henry Chao <henry.chao@accenture.com>
2019-07-11 13:49:02 -0400

    Setup of user route

---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+const index = async (req, res) => {
+  try {
+    // Connect to the DB
+  } catch (err) {
+    return res.status(400);
+  }
+};
+
+export default { index };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import users from "./users";
 
 const router = express.Router();
 
 // Establish a health check route
 router.get("/ping", (req, res) => res.send("pong"));
 
+router.use("/user", users);
+
 /*
-router.use("/user", userController);
 router.use("/login", loginController);
 router.use("/players", playersController);
 */
