95f26afcb8feb512c9821e64e28e4d4f517f2848
Henry Chao <henry.chao@accenture.com>
2019-07-12 11:47:31 -0400

    Added players and listing out

--- server/controllers/playersController.js
+++ server/controllers/playersController.js
@@ -1,4 +1,5 @@
 import { Player, ManagementRelationship } from "../models/index";
+import playerSerializer from "../serializers/playerSerializer";
 
 const index = async (req, res) => {
   try {
@@ -8,7 +9,19 @@ const index = async (req, res) => {
         adminUUID
       }
     });
-    console.log(adminRelationships);
+
+    let playersArray = [];
+    for (var rel in adminRelationships) {
+      const playerUUID = adminRelationships[rel].playerUUID;
+      const player = await Player.findByPk(playerUUID);
+      const playerData = playerSerializer(player);
+      playersArray.push(playerData);
+    }
+
+    return res.json({
+      success: true,
+      players: playersArray
+    });
   } catch (err) {
     return res.status(400).json({ success: false });
   }
@@ -16,9 +29,26 @@ const index = async (req, res) => {
 
 const create_player = async (req, res) => {
   try {
-    // Connect to the DB
+    const playerData = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      rating: req.body.rating,
+      handedness: req.body.handedness
+    };
+
+    const player = await Player.create(playerData);
+    const relationship = await ManagementRelationship.create({
+      adminUUID: req.body.user.id,
+      playerUUID: player.id
+    });
+    const playerCreated = playerSerializer(player);
+
+    return res.json({
+      success: true,
+      player: playerCreated
+    });
   } catch (err) {
-    return res.status(400);
+    return res.status(400).json({ success: false });
   }
 };
 
---
+++ server/serializers/playerSerializer.js
@@ -0,0 +1,8 @@
+const playerSerializer = ({ first_name, last_name, rating, handedness }) => ({
+  first_name,
+  last_name,
+  rating,
+  handedness
+});
+
+export default playerSerializer;
